2b55ff41402025bcc259444320b9eabd18a1c748
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2021-08-12 17:11:39 +0530

    Some Updatet

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ const database={
 }
 
 //1st:
-app.listen(3000,() => {
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
 
 //2nd:
